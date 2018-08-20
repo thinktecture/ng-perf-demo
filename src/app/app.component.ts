7823b2dcad9af9892d523ad5213794a31d1b0be7
Christian Liebel <christian.liebel@thinktecture.com>
2018-08-20 10:49:03 +0200

    feat: perf demo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ngPerf';
+  boxes: any[] = [];
+
+  addBox() {
+    this.boxes.push({});
+  }
+
+  addBoxes(amount: number) {
+    for (let i = 0; i < amount; i++) {
+      this.addBox();
+    }
+  }
 }
